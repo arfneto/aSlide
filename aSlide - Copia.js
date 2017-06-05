f53013ed8e0a6d1ce0bc2468aa31c09e651a8d26
arfneto <arfneto@users.noreply.github.com>
2017-06-05 00:45:56 -0300

    add control keys
    home, end and arrows

--- aSlide - Copia.js
+++ aSlide - Copia.js
@@ -11,7 +11,6 @@ function aSlide(
 	var sourceImage1 	= document.getElementById(idTop);
 	var sourceImage2 	= document.getElementById(idBottom);
 	var canvas 			= document.getElementById(idCanvas);
-
 	var _WheelUp		= ((Options[0] === "R")||(Options[0] === "r")) ? "Right" : "Left";
 	var _DivideNow		= ((Options[1] === "R")||(Options[0] === "r")) ? 2 : 0;
 
@@ -44,16 +43,12 @@ function aSlide(
 	var _Width = canvas.width;
 	var _Height = canvas.height;
 	var _wheelStep = Math.floor(_Width * 0.02); // pixels
-	var	nowMoving = false;
-	var	overCanvas = false;
 
-	// console.log("slide('17 arfneto@y7mail.com) image [" +
-	// 	 _Width + "x" + _Height + "]");
-	// console.log(
-	// 	"  Wheel: Step = " + _wheelStep +
-	// 	" Up: " + _WheelUp +
-	// 	" " + _DivideNow  + " button divide screen" 
-	// );
+	console.log("slide(2017 arfneto@y7mail.com) image size [" +
+		 _Width + "x" + _Height + "] wheel step = " + _wheelStep +
+		" Wheel Up goes " + _WheelUp +
+		" Divide Screen with mouse button " + _DivideNow 
+	);
 
 	var out = canvas.getContext('2d');
 
@@ -73,62 +68,18 @@ function aSlide(
 	// draw top image on screen
 	out.drawImage(iTopCanvas,0, 0, _Width, _Height);
 
+	var startX = 0;
+	var endX = 0;
 	var dvdr = 0;
-	
-	window.addEventListener(
-		"keyup",
-		function(e)
-		{
-			if(!overCanvas){ return };
-			if(e.key === "Home")
-			{
-				out.drawImage(iTopCanvas,0, 0, _Width, _Height);
-				dvdr = 0;
-				return;
-			}
-			if(e.key === "End")
-			{
-				out.drawImage(iBottomCanvas,0, 0, _Width, _Height);
-				dvdr = _Width;
-				return;
-			}
-			if(e.key === "ArrowLeft")
-			{
-				slideOver(-_wheelStep)
-				return;
-			}
-			if(e.key === "ArrowRight")
-			{
-				slideOver(_wheelStep)				
-				return;
-			}
-			if((e.key === "ArrowUp")||(e.key === "ArrowDown"))
-			{
-				nowDivide(Math.floor(_Width / 2));				
-				return;
-			}
-
-		}
-	);
-
+	 
 	canvas.addEventListener(
 		"click",
 		function(e)
 		{
 			if(e.button === _DivideNow)
 			{
-				nowDivide(e.clientX);
-			}
-		}
-	);
-
-	canvas.addEventListener(
-		"mousemove",
-		function(e)
-		{
-			if(nowMoving)
-			{
-				nowDivide(e.clientX);
+				startX = e.clientX;
+				nowDivide(startX);
 			}
 		}
 	);
@@ -137,8 +88,7 @@ function aSlide(
 		"mousedown",
 		function(e)
 		{
-			nowDivide(e.clientX);
-			nowMoving = true;
+			startX = e.clientX;
 		}
 	);
 
@@ -146,7 +96,6 @@ function aSlide(
 		"wheel",
 		function(e)
 		{
-			e.preventDefault();
 			if(_WheelUp === "Right")
 			{
 				(e.deltaY > 0) ? slideOver(-_wheelStep) : slideOver(_wheelStep);
@@ -162,9 +111,9 @@ function aSlide(
 		"mouseup",
 		function(e)
 		{
-			nowMoving = false;
+			endX = e.clientX;
 			if(e.type === "wheel"){ return };
-			if(e.button === _DivideNow){ nowDivide(e.clientX) }
+			(e.button === _DivideNow) ? nowDivide(endX) : slideOver(endX - startX);
 		}	// end fucntion()
 	);
 
@@ -172,29 +121,10 @@ function aSlide(
 		"mouseenter",
 		function(e)
 		{
-			overCanvas = true;
-		}	// end fucntion()
+			startX = 0;
+			endX = 0;
+		}
 	);
-
-	canvas.addEventListener(
-		"mouseleave",
-		function(e)
-		{
-			overCanvas = false;
-		}	// end fucntion()
-	);
-
-//
-// =======================================================================
-//
-
-	function dropLine(canvas, context,posX)
-	{
-		// code
-		var h = canvas.height;
-		context.fillStyle = "rgb(255,0,0)";
-		context.fillRect(posX, 0, 1, h);
-	}	// end function()
 
 	function slideOver(delta)
 	{
